288a1e7df674e376f1fcb4fff2031dd721f11d94
Jazz-S <jazz1santiago@gmail.com>
2015-03-11 15:19:21 -0700

    Jazz's First Drive

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -12,6 +12,7 @@
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -22,6 +23,25 @@
     
 }
 
+- (IBAction)onBackButtonPressed:(id)sender
+{
+
+    if (self.webView.canGoBack) {
+        [self.webView goBack];
+    }
+}
+- (IBAction)onForwardButtonPressed:(id)sender
+{
+    [self.webView goForward];
+}
+- (IBAction)onStopLoadingButtonPressed:(id)sender
+{
+    [self.webView stopLoading];
+}
+- (IBAction)onReloadButtonPressed:(id)sender
+{
+    [self.webView reload];
+}
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
@@ -32,5 +52,13 @@
     return YES;
 }
 
-
+#pragma mark UIWebViewDelegate Protocols
+-(void)webViewDidStartLoad:(UIWebView *)webView
+{
+    [self.activityIndicator startAnimating];
+}
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [self.activityIndicator stopAnimating];
+}
 @end
